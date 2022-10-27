e0479bf4241e15714e0ca4cce8f3d29127b39b4d
MattheWhite <fehermatyas97@gmail.com>
2022-10-27 22:10:10 +0200

    Implement Display.displayResult() method for printing out to the console.

--- src/main/java/com/codecool/leaguestatistics/view/Display.java
+++ src/main/java/com/codecool/leaguestatistics/view/Display.java
@@ -1,8 +1,17 @@
 package com.codecool.leaguestatistics.view;
 
+import com.codecool.leaguestatistics.model.Team;
+
 /**
  * Provides console view
  */
 public class Display {
-
+    public static void displayResult(Team team1, Team team2, int resultTeam1, int resultTeam2) {
+        StringBuilder display = new StringBuilder();
+        display.append(team1.getName()).append("\t")
+                .append(team2.getName()).append("\n")
+                .append(resultTeam1).append("\t")
+                .append(resultTeam2);
+        System.out.println(display);
+    }
 }
